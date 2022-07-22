ba2949fb3a84e6825816ac149330c6538b004846
David Lu <50630029+david1992121@users.noreply.github.com>
2022-07-22 11:19:40 +0800

    add validation on date field

--- works/serializers.py
+++ works/serializers.py
@@ -1,3 +1,4 @@
+from attr import validate
 from rest_framework import serializers
 from .models import Shift
 
@@ -12,3 +13,22 @@ class ShiftSerializer(serializers.ModelSerializer):
             'user': {'read_only': True}
         }
         model = Shift
+
+    def create(self, validated_data):
+        if not self.check_date(0, validated_data.get('user_id'), validated_data.get('date')):
+            raise serializers.ValidationError(
+                {'date': 'The shift already exists'})
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if 'date' in validated_data.keys():
+            if not self.check_date(instance.id, instance.user_id, validated_data.get('date')):
+                raise serializers.ValidationError(
+                    {'date': 'The shift already exists'})
+        return super().update(instance, validated_data)
+
+    def check_date(self, id, user_id, date):
+        queryset = Shift.objects.filter(user_id=user_id)
+        if int(id) > 0:
+            queryset = queryset.exclude(pk=id)
+        return queryset.filter(date=date).count() == 0
